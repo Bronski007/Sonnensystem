f69e989ade23a0b11be01cf62bd75a701ffe87ea
Bronski007 <152412743+Bronski007@users.noreply.github.com>
2025-07-15 11:24:46 +0200

    Small camera adjustments

--- core/initScene.js
+++ core/initScene.js
@@ -4,7 +4,7 @@ import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.m
 export function initScene() {
   const scene = new THREE.Scene();
 
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1500);
   camera.position.set(0, 0, 10);
 
   const renderer = new THREE.WebGLRenderer();
