4211b2bd1b771e803467f19fec98d0219c9c5057
Bronski007 <152412743+Bronski007@users.noreply.github.com>
2025-11-13 12:23:16 +0100

    Now using local file bodies.json instead of API due to authorization/CORS restrictions

--- API/planetAPI.js
+++ API/planetAPI.js
@@ -1,23 +1,25 @@
 /*
 Method to fetch data from https://api.le-systeme-solaire.net/
+
+Update: Now using local file bodies.json instead of API due to authorization/CORS restrictions
 */
 
 
 
-//Api communication - fetches the radius of a given planet their distance to the sun etc.
-//Source: Bro Code - "How to FETCH data from an API using JavaScript" link: https://www.youtube.com/watch?v=37vxWr0WgQk
+// Api communication - fetches the radius of a given planet their distance to the sun etc.
+// Source: Bro Code - "How to FETCH data from an API using JavaScript" link: https://www.youtube.com/watch?v=37vxWr0WgQk
 export async function fetchPlanetData(planet){
     try {
-        const response = await fetch("https://api.le-systeme-solaire.net/rest/bodies/"+planet);
-        if (!response.ok) {
-            throw new Error("could not fetch resource");
-        }
+        const response = await fetch("API/bodies.json");
         const data = await response.json();
+        const planetData = data.bodies.find(p => p.id.toLowerCase() === planet.toLowerCase() || (p.englishName && p.englishName.toLowerCase() === planet.toLowerCase()));
+
+        if (!planetData) throw new Error("Could not find planet!");
         return {
-            radius: data.meanRadius,
-            distanceToSun: data.semimajorAxis, // distance to the sun
-            eccentricity: data.eccentricity, // the degree of deviation from a perfect circle
-            sideralOrbit: data.sideralOrbit, // the time it takes for the planet to complete one orbit around the sun in earth days
+            radius: planetData.meanRadius,
+            distanceToSun: planetData.semimajorAxis, // distance to the sun
+            eccentricity: planetData.eccentricity, // the degree of deviation from a perfect circle
+            sideralOrbit: planetData.sideralOrbit, // the time it takes for the planet to complete one orbit around the sun in earth days
         };
     } catch (error) {
         console.error(error);
